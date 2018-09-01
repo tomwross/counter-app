96a7430fbba7e3b1d822eebced22f6702f7dc411
Tom Ross <sonofinterflux@gmail.com>
2018-08-31 23:47:17 -0400

    Repo initialization

---
+++ src/components/counter.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageSource: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>There are no items</p>;
+    }
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return <React.Fragment>{this.renderTags()}</React.Fragment>;
+  }
+}
+
+export default Counter;
